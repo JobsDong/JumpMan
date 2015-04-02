ca2818f444d1019424a1a1a4c88071b4263d07b9
JobsDong <wuyadong@shuimuhh.com>
2015-04-02 22:17:45 +0800

    add sprite sheet

--- game/setup.js
+++ game/setup.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function() {
 	var Q = window.Q = Quintus({development: true})
-		.include('Scenes, Sprites, 2D, Input')
+		.include('Scenes, Sprites, 2D, Anim, Input')
 		.include('GameScenes, GameSprites')
 		.setup({
 			width: 320,
@@ -8,7 +8,7 @@ window.addEventListener('load', function() {
 		}).controls();
 
     Q.load([
-        'background.png', 'stand_man.png', 'normal_brick.png'], function() {
+        'background.png', 'sprites.png', 'normal_brick.png'], function() {
             Q.stageScene('Background');
             Q.stageScene('Level', 1);
         });
